34bb56b9ef6c251267d7b54ce81b04198f75dae8
SofiaSalina <Lourdes.SalinaSalas@alu.frt.utn.edu.ar>
2026-05-22 18:54:01 -0300

    feat: completar los 4 casos

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +10,40 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    private List<Alumno> listaAlumnos = new List<Alumno>();
+
+    public void AgregarAlumno(Alumno alumno)
+    {
+        listaAlumnos.Add(alumno);
+    }
+
+    public List<Alumno> ObtenerLista()
+    {
+        return listaAlumnos;
+    }
+
+    public Alumno? BuscarPorNombre(string nombre)
+    {
+        foreach (Alumno alumno in listaAlumnos)
+        {
+            if (alumno.Nombre == nombre)
+            {
+                return alumno;
+            }
+        }
+        return null;
+    }
+
+    public void EliminarAlumno(Alumno alumno)
+    {
+        listaAlumnos.Remove(alumno);
+    }
+
+    public void EliminarEnPosicion(int index)
+    {
+        if (index >= 0 && index < listaAlumnos.Count)
+        {
+            listaAlumnos.RemoveAt(index);
+        }
+    }
 }
